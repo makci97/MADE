7a47f384c2a3652f80a88f3ad7d780f8c3edee60
Maxim <makci.97@mail.ru>
2020-05-01 18:43:52 +0300

    Add CropAugmenter

--- contest1/unsupervised-landmarks-thousand-landmarks-contest/augmentation.py
+++ contest1/unsupervised-landmarks-thousand-landmarks-contest/augmentation.py
@@ -85,4 +85,89 @@ class AffineAugmenter(object):
         inv_rotation = self.inv_rotation_matrix(rotation)
         
         return self._apply_rotation(sample=sample, rotation=inv_rotation)
+    
+    
+class CropAugmenter(object):
+    def __init__(self, min_scale=0.8, with_inv=False, elem_name='image'):
+        self._min_scale = min_scale
+        self.elem_name = elem_name
+        self._with_inv = with_inv
+        
+    def _get_params(self, sample):
+        params = {}
+        h, w, c = sample[self.elem_name].shape
+        
+        params['scale'] = self._min_scale + random.random() * (1 - self._min_scale)
+        params['new_w'] = int(params['scale'] * w)
+        params['new_h'] = int(params['scale'] * h)
+        
+        params['x_offset'] = random.randint(0, w - params['new_w'])
+        params['y_offset'] = random.randint(0, h - params['new_h'])
+        
+        return params
+    
+    def __call__(self, sample):
+        params = self._get_params(sample)
+
+        image = np.array(sample[self.elem_name])
+        h, w, c = image.shape
+        
+        image = cv2.resize(image[
+                params['y_offset']:params['y_offset'] + params['new_h'], 
+                params['x_offset']:params['x_offset'] + params['new_w']
+            ], 
+            (w, h),
+        )
+        sample[self.elem_name] = image
+        
+        if 'landmarks' in sample:
+            landmarks = sample['landmarks'].reshape(-1, 2)
+            landmarks[:, 0] = (landmarks[:, 0] - params['x_offset']) * w / params['new_w']  # x
+            landmarks[:, 1] = (landmarks[:, 1] - params['y_offset']) * h / params['new_h']  # y
+            
+            sample['landmarks'] = landmarks.reshape(-1)
+        
+        if self._with_inv:
+            sample['crop_params'] = params
+        
+        return sample
+    
+    def inv(self, sample):
+        params = sample['crop_params']#{k: float(v) for k, v in sample['crop_params'].items()}
+        
+        image = np.array(sample[self.elem_name])
+        h, w, c = image.shape
+        
+        small_image = cv2.resize(image, (params['new_w'], params['new_h']))
+        inv_image = np.full((h, w, c), 128, dtype=np.uint8)
+        inv_image[
+            params['y_offset']:params['y_offset'] + params['new_h'], 
+            params['x_offset']:params['x_offset'] + params['new_w'],
+            :
+        ] = small_image
+        sample[self.elem_name] = inv_image
+        
+        if 'landmarks' in sample:
+            landmarks = sample['landmarks'].reshape(-1, 2)
+            landmarks[:, 0] = landmarks[:, 0] * params['new_w'] / w + params['x_offset']  # x
+            landmarks[:, 1] = landmarks[:, 1] * params['new_h'] / h + params['y_offset']  # y
+            
+            sample['landmarks'] = landmarks.reshape(-1)
+        
+        return sample
+    
+    
+def get_i_from_dict(i: int, d: dict):
+    sample = {}
+    for key, val in d.items():
+        if isinstance(val, dict):
+            sample[key] = get_i_from_dict(i, val)
+        else:
+            sample[key] = val[i]
+    return sample
+
+
+def batch2samples(batch, size):
+    for i in range(size):
+        yield get_i_from_dict(i, batch)
         
